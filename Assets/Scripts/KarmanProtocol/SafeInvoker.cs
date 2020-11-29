59fc5f9f54a2fb9bd842391ff3a7e82138de7395
Simon Karman <simonkarman@hotmail.com>
2020-11-29 22:40:23 +0100

    Added action null check to SafeInvoker

--- Assets/Scripts/KarmanProtocol/SafeInvoker.cs
+++ Assets/Scripts/KarmanProtocol/SafeInvoker.cs
@@ -10,12 +10,14 @@ namespace KarmanProtocol {
 
         public static bool Invoke(Logger log, string name, Action action) {
             bool allSucceeded = true;
-            foreach (Delegate invocation in action.GetInvocationList()) {
-                try {
-                    (invocation as Action)?.Invoke();
-                } catch (Exception ex) {
-                    allSucceeded = false;
-                    LogExceptionError(log, name, ex);
+            if (action != null) {
+                foreach (Delegate invocation in action.GetInvocationList()) {
+                    try {
+                        (invocation as Action)?.Invoke();
+                    } catch (Exception ex) {
+                        allSucceeded = false;
+                        LogExceptionError(log, name, ex);
+                    }
                 }
             }
             return allSucceeded;
@@ -23,12 +25,14 @@ namespace KarmanProtocol {
 
         public static bool Invoke<T>(Logger log, string name, Action<T> action, T obj) {
             bool allSucceeded = true;
-            foreach (Delegate invocation in action.GetInvocationList()) {
-                try {
-                    (invocation as Action<T>)?.Invoke(obj);
-                } catch (Exception ex) {
-                    allSucceeded = false;
-                    LogExceptionError(log, name, ex);
+            if (action != null) {
+                foreach (Delegate invocation in action.GetInvocationList()) {
+                    try {
+                        (invocation as Action<T>)?.Invoke(obj);
+                    } catch (Exception ex) {
+                        allSucceeded = false;
+                        LogExceptionError(log, name, ex);
+                    }
                 }
             }
             return allSucceeded;
@@ -36,12 +40,14 @@ namespace KarmanProtocol {
 
         public static bool Invoke<T1, T2>(Logger log, string name, Action<T1, T2> action, T1 obj1, T2 obj2) {
             bool allSucceeded = true;
-            foreach (Delegate invocation in action.GetInvocationList()) {
-                try {
-                    (invocation as Action<T1, T2>)?.Invoke(obj1, obj2);
-                } catch (Exception ex) {
-                    allSucceeded = false;
-                    LogExceptionError(log, name, ex);
+            if (action != null) {
+                foreach (Delegate invocation in action.GetInvocationList()) {
+                    try {
+                        (invocation as Action<T1, T2>)?.Invoke(obj1, obj2);
+                    } catch (Exception ex) {
+                        allSucceeded = false;
+                        LogExceptionError(log, name, ex);
+                    }
                 }
             }
             return allSucceeded;
@@ -49,12 +55,14 @@ namespace KarmanProtocol {
 
         public static bool Invoke<T1, T2, T3>(Logger log, string name, Action<T1, T2, T3> action, T1 obj1, T2 obj2, T3 obj3) {
             bool allSucceeded = true;
-            foreach (Delegate invocation in action.GetInvocationList()) {
-                try {
-                    (invocation as Action<T1, T2, T3>)?.Invoke(obj1, obj2, obj3);
-                } catch (Exception ex) {
-                    allSucceeded = false;
-                    LogExceptionError(log, name, ex);
+            if (action != null) {
+                foreach (Delegate invocation in action.GetInvocationList()) {
+                    try {
+                        (invocation as Action<T1, T2, T3>)?.Invoke(obj1, obj2, obj3);
+                    } catch (Exception ex) {
+                        allSucceeded = false;
+                        LogExceptionError(log, name, ex);
+                    }
                 }
             }
             return allSucceeded;
